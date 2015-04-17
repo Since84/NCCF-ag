a038b44cb44930839d5e45ac42a2b1dcf8a48db3
Damon <damonjhastins@gmail.com>
2015-04-17 11:04:17 -0700

    Transition fix

--- wp-content/themes/Spark15_NCCF/page-team.php
+++ wp-content/themes/Spark15_NCCF/page-team.php
@@ -4,6 +4,14 @@ get_header();
 $context 	= Timber::get_context();
 $post		= new TimberPost();
 
+// Header 
+$headerContext 	= array(
+	"logo"		=>	get_header_image()
+	,"nav"		=>	new TimberMenu( "main-nav" )
+);
+$headerContext = array_merge($headerContext, $context);
+$context["header"] = Timber::compile("view/section/header.twig", $headerContext);
+
 // Team Accordion 
 $teamPost = new TimberPost("Team");
 $teamFeedContext = array( 
